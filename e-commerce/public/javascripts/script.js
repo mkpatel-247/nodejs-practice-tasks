2aae8fdc507f1c47914871f66750e49e998ad4d7
mkpatel-247 <meet.codes247@gmail.com>
2024-10-11 19:01:39 +0530

    Completed basic handlebars activity.

--- e-commerce/public/javascripts/script.js
+++ e-commerce/public/javascripts/script.js
@@ -1,7 +1,20 @@
 $(function () {
-    $(document).on("click", "#increaseItem", function () {
+    /**
+     * When click on `+` button.
+     */
+    $(document).on("click", "#btn-plus", function () {
         const currentQuantity = document.getElementById("product-quantity");
-        console.log("currentQuantity :>> ", currentQuantity);
-        // currentQuantity += 1;
+        console.log("currentQuantity :>> ", currentQuantity.value);
+        currentQuantity.value = parseInt(currentQuantity.value) + 1;
+    });
+    /**
+     * When click on `-` button.
+     */
+    $(document).on("click", "#btn-minus", function () {
+        const currentQuantity = document.getElementById("product-quantity");
+        console.log("currentQuantity :>> ", currentQuantity.value);
+        if (currentQuantity.value > 0) {
+            currentQuantity.value = parseInt(currentQuantity.value) - 1;
+        }
     });
 });
--- e-commerce/routes/product.route.js
+++ e-commerce/routes/product.route.js
@@ -10,12 +10,31 @@ router.get("/", (req, res, next) => {
     res.render("index", { products: productList });
 });
 
+/**
+ * Product detail route.
+ */
 router.get("/product/:id", (req, res, next) => {
     const { id } = req.params;
     const detail = getProductById(id);
     res.render("product-detail", {
         productDetail: detail,
     });
+});
+
+/**
+ * Add to cart.
+ */
+router.get("/add-to-cart/:id", (req, res, next) => {
+    const { id } = req.params;
+    // const detail = getProductById(id);
+    res.redirect(`product/${id}`);
+});
+
+/**
+ * Cart page.
+ */
+router.get("/cart", (req, res, next) => {
+    res.render("cart");
 });
 
 export default router;
